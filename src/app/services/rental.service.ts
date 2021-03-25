6aca8c7f81cb1c54d72425418dfc473608d6d36c
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-03-25 05:42:13 +0200

    List

---
+++ src/app/services/rental.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RentalResponseModel } from '../models/rentalResponseModel';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RentalService {
+  apiUrl = 'https://localhost:44333/api/rentals/getall';
+  constructor(private httpClient: HttpClient) {}
+
+  getRentals(): Observable<RentalResponseModel> {
+    return this.httpClient.get<RentalResponseModel>(this.apiUrl);
+  }
+}
