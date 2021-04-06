bf5a233abe00cdb094e4b33541fc034823d05e11
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-04-06 03:34:58 +0300

    Findeks Service

---
+++ src/app/services/findeks.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Findeks } from '../models/findeks';
+import { ListResponseModel } from '../models/listResponseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FindeksService {
+  apiUrl = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getByCustomerId(customerId: number): Observable<SingleResponseModel<Findeks>> {
+    let newPath=this.apiUrl+"findeks/getbycustomerid?customerId="+customerId
+    return this.httpClient.get<SingleResponseModel<Findeks>>(newPath);
+  
+  }
+}
